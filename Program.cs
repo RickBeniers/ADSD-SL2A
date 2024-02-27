0fb707baabbc69c2f098227635d5e2bcc0e974b9
RickBeniers <Beniersrick@gmail.com>
2024-02-27 09:28:22 +0100

    first commit. 27-02-2024

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADSD_SL2A_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
